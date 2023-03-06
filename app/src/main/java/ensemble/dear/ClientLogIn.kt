9c9a82f2468fe72a93f312d92ec35f2bd40ad767
eliseobao <eliseo.bao@udc.es>
2023-03-06 12:21:52 +0100

    Client Log In UI

---
+++ app/src/main/java/ensemble/dear/ClientLogIn.kt
@@ -0,0 +1,11 @@
+package ensemble.dear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClientLogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client_log_in)
+    }
+}
