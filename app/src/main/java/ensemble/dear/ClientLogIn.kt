91fdd1af8e19fe99f870d40ca78fa206eb1bdf76
eliseobao <eliseo.bao@udc.es>
2023-03-10 16:35:00 +0100

    Initial version for Client Log In (#11)

--- app/src/main/java/ensemble/dear/ClientLogIn.kt
+++ app/src/main/java/ensemble/dear/ClientLogIn.kt
@@ -1,11 +1,25 @@
 package ensemble.dear
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class ClientLogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_log_in)
+
+        val courierLogInButton = findViewById<Button>(R.id.buttonAuthorizedCouriers)
+        courierLogInButton.setOnClickListener{
+            val intent = Intent(this, CourierLogIn::class.java)
+            startActivity(intent)
+        }
+
+        val googleLogIn = findViewById<Button>(R.id.buttonLogInGoogle)
+        googleLogIn.setOnClickListener{
+            Toast.makeText(applicationContext, "Opening Google OAuth", Toast.LENGTH_LONG).show()
+        }
     }
 }
