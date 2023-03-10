1cb8983e75be8e31d2e7a4fab1b281c1112ae328
DiegoMartinezVillar <diego.martinez.villar@udc.es>
2023-03-10 18:05:42 +0100

    Added navigation for ClientTrackingDetails and ClientTrackingDetailsMap (#11)

--- app/src/main/java/ensemble/dear/ClientTrackingDetails.kt
+++ app/src/main/java/ensemble/dear/ClientTrackingDetails.kt
@@ -1,11 +1,34 @@
 package ensemble.dear
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ClientTrackingDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_tracking_details)
+
+        val locateButton = findViewById<Button>(R.id.buttonLocate)
+        locateButton.setOnClickListener{
+            val intent = Intent(applicationContext, CourierLogIn::class.java)
+            startActivity(intent)
+        }
+
+        val chatButton = findViewById<FloatingActionButton>(R.id.buttonChat)
+        chatButton.setOnClickListener{
+            val intent = Intent(applicationContext, Chat::class.java)
+            startActivity(intent)
+        }
+
+        val phoneNumberCallText = findViewById<TextView>(R.id.textPhoneNumberCall)
+        phoneNumberCallText.setOnClickListener{
+            Toast.makeText(applicationContext, "Calling "+ phoneNumberCallText.text, Toast.LENGTH_LONG).show()
+        }
+
     }
 }
--- app/src/main/java/ensemble/dear/ClientTrackingDetailsMap.kt
+++ app/src/main/java/ensemble/dear/ClientTrackingDetailsMap.kt
@@ -1,11 +1,27 @@
 package ensemble.dear
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ClientTrackingDetailsMap : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_tracking_details_map)
+
+        val chatButton = findViewById<FloatingActionButton>(R.id.buttonChat)
+        chatButton.setOnClickListener{
+            val intent = Intent(applicationContext, Chat::class.java)
+            startActivity(intent)
+        }
+
+        val phoneNumberCallText = findViewById<TextView>(R.id.textPhoneNumberCall)
+        phoneNumberCallText.setOnClickListener{
+            Toast.makeText(applicationContext, "Calling "+ phoneNumberCallText.text, Toast.LENGTH_LONG).show()
+        }
+
     }
 }
