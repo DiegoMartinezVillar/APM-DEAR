33d87641db61b9dca34ffe61b1aee7939f128c90
eliseobao <eliseo.bao@udc.es>
2023-03-11 13:08:16 +0100

    Finished linking Chat (#11)

--- app/src/main/java/ensemble/dear/Chat.kt
+++ app/src/main/java/ensemble/dear/Chat.kt
@@ -2,10 +2,18 @@ package ensemble.dear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class Chat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        val buttonChatSendMessage = findViewById<ImageView>(R.id.buttonChatSendMessage)
+        buttonChatSendMessage.setOnClickListener{
+            Toast.makeText(applicationContext, "Sending message", Toast.LENGTH_LONG).show()
+        }
     }
 }
