892d4536b339be4e46c97c153d6541ea610072ca
eliseobao <eliseo.bao@udc.es>
2023-03-11 13:13:38 +0100

    Finished linking Profile (#11)

--- app/src/main/java/ensemble/dear/Profile.kt
+++ app/src/main/java/ensemble/dear/Profile.kt
@@ -1,11 +1,19 @@
 package ensemble.dear
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val profilePastTracking = findViewById<LinearLayout>(R.id.profilePastTracking)
+        profilePastTracking.setOnClickListener{
+            startActivity(Intent(applicationContext, ClientTrackingDetails::class.java))
+        }
     }
 }
