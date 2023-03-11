813bebe1e6b494aba5d400655a0e8000070b0485
mariaisblue <55807153+mariaisblue@users.noreply.github.com>
2023-03-11 19:48:14 +0100

    added missing PendingShipments activity (#11)

--- app/src/main/java/ensemble/dear/CourierLogIn.kt
+++ app/src/main/java/ensemble/dear/CourierLogIn.kt
@@ -1,5 +1,6 @@
 package ensemble.dear
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,7 +13,7 @@ class CourierLogIn : AppCompatActivity() {
 
         val buttonCourierLogIn = findViewById<Button>(R.id.buttonCourierLogIn)
         buttonCourierLogIn.setOnClickListener{
-            Toast.makeText(applicationContext, "Pending shipments", Toast.LENGTH_LONG).show()
+            startActivity(Intent(applicationContext, PendingShipments::class.java))
         }
     }
 }
---
+++ app/src/main/java/ensemble/dear/PendingShipments.kt
@@ -0,0 +1,11 @@
+package ensemble.dear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PendingShipments : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pending_shipments)
+    }
+}
