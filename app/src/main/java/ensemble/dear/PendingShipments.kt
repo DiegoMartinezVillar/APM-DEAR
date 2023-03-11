d2a2da46b3cd8b27bc1fbd8e11616f285939d2a7
mariaisblue <55807153+mariaisblue@users.noreply.github.com>
2023-03-11 20:06:47 +0100

    navigation for PendingShipments (#11)

--- app/src/main/java/ensemble/dear/PendingShipments.kt
+++ app/src/main/java/ensemble/dear/PendingShipments.kt
@@ -1,11 +1,28 @@
 package ensemble.dear
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class PendingShipments : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pending_shipments)
+
+        val buttonScan = findViewById<Button>(R.id.buttonScan)
+
+        val textAddressPackage = findViewById<TextView>(R.id.textAddressPackage)
+
+        buttonScan.setOnClickListener {
+            startActivity(Intent(applicationContext, ARScanner::class.java))
+        }
+
+        textAddressPackage.setOnClickListener {
+            startActivity(Intent(applicationContext, CourierTrackingDetails::class.java))
+        }
+
+
     }
 }
