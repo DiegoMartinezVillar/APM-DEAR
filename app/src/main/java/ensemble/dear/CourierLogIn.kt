1b6ce2e7dac7a6b8bc2b721987a1585efafa6b4b
eliseobao <eliseo.bao@udc.es>
2023-03-11 13:03:51 +0100

    WIP Courier Log In (No Pending Shipments Activity) (#11)

--- app/src/main/java/ensemble/dear/CourierLogIn.kt
+++ app/src/main/java/ensemble/dear/CourierLogIn.kt
@@ -2,10 +2,17 @@ package ensemble.dear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class CourierLogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_courier_log_in)
+
+        val buttonCourierLogIn = findViewById<Button>(R.id.buttonCourierLogIn)
+        buttonCourierLogIn.setOnClickListener{
+            Toast.makeText(applicationContext, "Pending shipments", Toast.LENGTH_LONG).show()
+        }
     }
 }
