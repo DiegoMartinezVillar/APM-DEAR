d8e472666e55d84405c98dbaf78140f68e115672
eliseobao <eliseo.bao@udc.es>
2023-03-11 12:53:38 +0100

    Finished linking Client Sign Up (#11)

--- app/src/main/java/ensemble/dear/ClientSignUp.kt
+++ app/src/main/java/ensemble/dear/ClientSignUp.kt
@@ -1,11 +1,18 @@
 package ensemble.dear
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ClientSignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_sign_up)
+
+        val buttonClientSignUp = findViewById<Button>(R.id.buttonClientSignUp)
+        buttonClientSignUp.setOnClickListener{
+            startActivity(Intent(applicationContext, CurrentTrackings::class.java))
+        }
     }
 }
