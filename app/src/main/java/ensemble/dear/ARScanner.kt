94b41498ea32426cbc7b12208d1d836466083550
mariaisblue <55807153+mariaisblue@users.noreply.github.com>
2023-03-13 19:50:58 +0100

    navigation for AddTracking + ARScanner (#11)

--- app/src/main/java/ensemble/dear/ARScanner.kt
+++ app/src/main/java/ensemble/dear/ARScanner.kt
@@ -2,11 +2,20 @@ package ensemble.dear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ARScanner : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arscanner)
+
+        val cameraButton = findViewById<FloatingActionButton>(R.id.cameraButton)
+
+        cameraButton.setOnClickListener {
+            Toast.makeText(applicationContext, "open camera", Toast.LENGTH_LONG).show()
+        }
     }
 
 }
--- app/src/main/java/ensemble/dear/AddTracking.kt
+++ app/src/main/java/ensemble/dear/AddTracking.kt
@@ -1,11 +1,28 @@
 package ensemble.dear
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class AddTracking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_tracking)
+
+        val buttonAddTracking = findViewById<Button>(R.id.add_button)
+        val buttonSearchButton = findViewById<Button>(R.id.search_button)
+
+        buttonAddTracking.setOnClickListener {
+            startActivity(Intent(applicationContext, CurrentTrackings::class.java))
+        }
+
+        buttonSearchButton.setOnClickListener {
+            Toast.makeText(applicationContext,
+                android.R.string.search_go, Toast.LENGTH_LONG).show()
+        }
+
     }
 }
