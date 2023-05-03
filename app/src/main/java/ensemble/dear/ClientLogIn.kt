cac026e169837d252aaf80d958842ae7979bf47f
eliseobao <eliseo.bao@udc.es>
2023-05-03 16:22:47 +0200

    Courier auth ready but WIP with COURIER_EMAILS constant

--- app/src/main/java/ensemble/dear/ClientLogIn.kt
+++ app/src/main/java/ensemble/dear/ClientLogIn.kt
@@ -1,10 +1,12 @@
 package ensemble.dear
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -13,19 +15,25 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import ensemble.dear.currentTrackings.CurrentTrackings
+import ensemble.dear.pendingShipments.PendingShipments
+
+object Constants {
+    val COURIER_EMAILS = listOf("ensemble.dear.app@gmail.com")
+}
 
 
 class ClientLogIn : AppCompatActivity() {
 
     lateinit var gso: GoogleSignInOptions
     lateinit var gsc: GoogleSignInClient
-
+    lateinit var attemptType: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_log_in)
 
-        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
         gsc = GoogleSignIn.getClient(this, gso)
 
         val acct = GoogleSignIn.getLastSignedInAccount(this)
@@ -39,41 +47,68 @@ class ClientLogIn : AppCompatActivity() {
 
         val buttonClientLogIn = findViewById<Button>(R.id.buttonClientLogIn)
         buttonClientLogIn.setOnClickListener {
-            startActivity(Intent(applicationContext, CurrentTrackings::class.java))
+            //startActivity(Intent(applicationContext, CurrentTrackings::class.java))
+            Toast.makeText(this, "Non-priority feature.", Toast.LENGTH_SHORT).show()
         }
 
         val buttonClientLogInGoogle = findViewById<Button>(R.id.buttonClientLogInGoogle)
         buttonClientLogInGoogle.setOnClickListener {
-            startActivityForResult(gsc.signInIntent, 1000)
+            resultLauncher.launch(gsc.signInIntent)
+            attemptType = "client"
         }
 
         val buttonAreYouAuthorizedCourier = findViewById<Button>(R.id.buttonAreYouAuthorizedCourier)
         buttonAreYouAuthorizedCourier.setOnClickListener {
-            startActivity(Intent(applicationContext, CourierLogIn::class.java))
+            resultLauncher.launch(gsc.signInIntent)
+            attemptType = "courier"
         }
 
         val textViewClickHereToSignUp = findViewById<TextView>(R.id.textViewClickHereToSignUp)
         textViewClickHereToSignUp.setOnClickListener {
-            startActivity(Intent(applicationContext, ClientSignUp::class.java))
+            //startActivity(Intent(applicationContext, ClientSignUp::class.java))
+            Toast.makeText(this, "Non-priority feature.", Toast.LENGTH_SHORT).show()
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1000) {
-            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                task.getResult(ApiException::class.java)
-                navigateToSecondActivity()
-            } catch (e: ApiException) {
-                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT)
-                    .show()
+    private var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val task: Task<GoogleSignInAccount> =
+                    GoogleSignIn.getSignedInAccountFromIntent(data)
+                try {
+                    task.getResult(ApiException::class.java)
+                    val email = GoogleSignIn.getLastSignedInAccount(this)?.email
+                    if ((attemptType == "courier") and (email !in Constants.COURIER_EMAILS)) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Email not recognized as courier",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        gsc.signOut()
+                    } else if ((attemptType == "client") and (email in Constants.COURIER_EMAILS)) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Email only valid for courier login",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        gsc.signOut()
+                    } else {
+                        navigateToSecondActivity()
+                    }
+                } catch (e: ApiException) {
+                    Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
         }
-    }
 
     private fun navigateToSecondActivity() {
         finish()
-        startActivity(Intent(applicationContext, CurrentTrackings::class.java))
+        if (GoogleSignIn.getLastSignedInAccount(this)?.email in Constants.COURIER_EMAILS) {
+            startActivity(Intent(applicationContext, PendingShipments::class.java))
+        } else {
+            startActivity(Intent(applicationContext, CurrentTrackings::class.java))
+        }
     }
 }
