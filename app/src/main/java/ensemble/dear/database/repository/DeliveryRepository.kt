4c777bb08e72c8a0c59aa1ca290dd97b56baf968
mariaisblue <55807153+mariaisblue@users.noreply.github.com>
2023-05-04 23:55:09 +0200

    merge (#43)

--- app/src/main/java/ensemble/dear/database/repository/DeliveryRepository.kt
+++ app/src/main/java/ensemble/dear/database/repository/DeliveryRepository.kt
@@ -1,22 +1,15 @@
 package ensemble.dear.database.repository
 
-import android.app.Application
 import android.content.Context
-import android.os.AsyncTask
-import androidx.lifecycle.LiveData
 import ensemble.dear.database.AppDatabase
 import ensemble.dear.database.dao.DeliveryDAO
-import ensemble.dear.database.dao.PackageDAO
 import ensemble.dear.database.entities.DeliveryEntity
-import ensemble.dear.database.entities.PackageEntity
 
-class DeliveryRepository(application: Application) {
+class DeliveryRepository(context: Context) {
 
+    //private val database = AppDatabase.getInstance(application)
 
-
-    private val database = AppDatabase.getInstance(application)
-
-    var deliveriesDAO: DeliveryDAO = AppDatabase.getInstance(context)?.deliveriesDAO()!!
+    var deliveriesDAO: DeliveryDAO = AppDatabase.getInstance(context)?.deliveryDAO()!!
 
 
     fun insert(delivery: DeliveryEntity) {
